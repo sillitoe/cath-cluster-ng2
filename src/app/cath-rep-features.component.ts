bae6a03150c4d6c9bb084996bb129b382862a11f
Ian Sillitoe <ian@sillit.com>
2017-03-14 22:45:25 +0000

    add placeholders

--- src/app/cath-rep-features.component.ts
+++ src/app/cath-rep-features.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cath-rep-features',
   template: `
-    <div class="">CATH Rep Features
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Options</h4>
+        <div class="card-text">
+          CATH rep features
+        </div>
+      </div>
     </div>
   `
 })
--- src/app/cath-rep.component.ts
+++ src/app/cath-rep.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cath-rep',
@@ -9,5 +9,6 @@ import { Component } from '@angular/core';
 })
 
 export class CathRepComponent {
-  
+  @Input()
+  repid: string;
 }

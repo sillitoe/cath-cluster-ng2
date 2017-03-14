54fa74a98c55f32948be16c9c1050507b85c8ff9
Ian Sillitoe <ian@sillit.com>
2017-03-14 22:39:10 +0000

    add cluster members

---
+++ src/app/cath-cluster-members.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cath-cluster-members',
+  template: `
+    <div>
+      CATH Cluster Members
+    </div>
+  `
+})
+
+export class CathClusterMembersComponent {
+  
+}
